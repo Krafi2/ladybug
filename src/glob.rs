b1c6f19f1bdd593d7b5e5d61e68377bcc3ec0fee
Krafi <jakub.havlicek5@gmail.com>
2021-10-12 22:05:01 +0200

    Removed unused code

--- src/glob.rs
+++ src/glob.rs
@@ -30,27 +30,3 @@ pub fn new_walker(globs: Override) -> WalkBuilder {
     builder.overrides(globs).standard_filters(false);
     builder
 }
-
-// pub mod de {
-//     use ignore::overrides::{Override, OverrideBuilder};
-//     use serde::{de::Error, Deserialize, Deserializer};
-//
-//     pub fn pattern<'de, D>(deserializer: D) -> Result<Override, D::Error>
-//     where
-//         D: Deserializer<'de>,
-//     {
-//         let string = <String as Deserialize>::deserialize(deserializer)?;
-//         Pattern::new(&string).map_err(D::Error::custom)
-//     }
-//
-//     pub fn patterns<'de, D>(deserializer: D) -> Result<Override, D::Error>
-//     where
-//         D: Deserializer<'de>,
-//     {
-//         let vec = <Vec<String> as Deserialize>::deserialize(deserializer)?;
-//         vec.iter()
-//             .map(|s| Pattern::new(&s))
-//             .collect::<Result<Vec<_>, _>>()
-//             .map_err(D::Error::custom)
-//     }
-// }
