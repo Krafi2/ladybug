05c0497a1ae9a5f558024bc571b718ce796b74f8
Krafi <jakub.havlicek5@gmail.com>
2023-03-20 19:37:35 +0100

    Implement flatpak provider

--- interpreter/src/provider/flatpak.rs
+++ interpreter/src/provider/flatpak.rs
@@ -1,53 +1,282 @@
-use parser::span::AriadneSpan;
+use std::{
+    io::{BufRead, BufReader},
+    process::{Command, Stdio},
+    rc::Rc,
+};
 
-use crate::error::IntoReport;
+use ariadne::{Color, Fmt, ReportKind};
+use color_eyre::{
+    eyre::{bail, eyre, WrapErr},
+    Result, Section,
+};
+use common::command::{check_output, run_command};
 
-use super::ExecutionCtx;
+use crate::{
+    structures::{FromArgs, RecoverFromArgs},
+    Span,
+};
+
+use super::{ExecutionCtx, ProviderError};
 
 #[derive(Debug)]
-pub struct Error;
+pub enum Error {
+    PackageNotRecognized(String, Span),
+    AmbiguousPackage(String, Span),
+    Eyre(color_eyre::Report, Span),
+}
 
-impl IntoReport for Error {
-    fn into_report(self, _filename: &str) -> ariadne::Report<AriadneSpan> {
-        todo!()
+impl Into<crate::error::Error> for Error {
+    fn into(self) -> crate::error::Error {
+        super::TransactionError::Flatpak(self).into()
     }
 }
 
-pub struct Provider {}
+report! {
+    Error {
+        Error::PackageNotRecognized(package, span) => {
+            report(ReportKind::Error, span.start);
+            message("Package '{}' doesn't exist", package.fg(Color::Red));
+            label(span, Color::Red, "Couldn't find this package");
+        }
+        Error::AmbiguousPackage(package, span) => {
+            report(ReportKind::Error, span.start);
+            message("Package name '{}' is ambiguous", package.fg(Color::Red));
+            label(span, Color::Red, "Multiple results found");
+        }
+        Error::Eyre(err, span) => {
+            report(ReportKind::Error, span.start);
+            message("Encountered an error while proccessing package");
+            label(span, Color::Red, "{err}");
+        }
+    }
+}
+
+/// The flatpak provider is stateless
+pub struct Provider;
 
 impl super::ConstructProvider for Provider {
-    fn new(_root: bool) -> Result<Self, super::ProviderError> {
-        todo!()
+    fn new(_root: bool) -> Result<Self, ProviderError> {
+        // try to run 'flatpak --version' to see whether flatpak is installed
+        let out = Command::new("flatpak")
+            .arg("--version")
+            .stdin(Stdio::null())
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .wrap_err("Failed to spawn flatpak process")
+            .and_then(|mut cmd| cmd.wait().wrap_err("Failed to run flatpak"));
+
+        match out {
+            Ok(status) => {
+                if status.success() {
+                    Ok(Self)
+                } else {
+                    Err(ProviderError::Unavailable(Rc::new(eyre!(
+                        "Flatpak exited with exit code {status}"
+                    ))))
+                }
+            }
+            Err(err) => Err(ProviderError::Unavailable(Rc::new(err))),
+        }
     }
+}
+
+params! {
+    struct Params {
+        user: Option<bool>,
+    }
+}
+params! {
+    struct PackageParams {}
+}
+
+struct Package {
+    name: String,
+}
+
+pub struct Transaction {
+    /// Work on user installation
+    user: bool,
+    packages: Vec<Package>,
 }
 
 impl super::Provider for Provider {
-    type Transaction = ();
+    type Transaction = Transaction;
     type State = ();
 
     fn new_transaction(
         &mut self,
-        _args: crate::Args,
-        _packages: crate::Packages,
-        _ctx: &mut crate::eval::Ctx,
+        args: crate::Args,
+        packages: crate::Packages,
+        ctx: &mut crate::eval::Ctx,
     ) -> Result<Self::Transaction, ()> {
-        todo!()
+        // Parse the package block params
+        let params = Params::recover_default(args, ctx);
+        let user = params.user.unwrap_or_default();
+
+        let mut packs = Vec::new();
+        let mut degraded = params.is_degraded();
+        // Verify packages
+        for package in packages.packages {
+            if let Some(args) = PackageParams::from_args(package.args, ctx) {
+                if args.is_degraded() {
+                    degraded = true;
+                    continue;
+                }
+
+                match exists(&package.name.inner, user) {
+                    Ok(PackageStatus::Ok) => {
+                        let package = Package {
+                            name: package.name.inner,
+                        };
+                        packs.push(package);
+                    }
+                    Ok(PackageStatus::Ambiguous) => {
+                        ctx.emit(Error::AmbiguousPackage(
+                            package.name.inner,
+                            package.name.span,
+                        ));
+                        degraded = true;
+                    }
+                    Ok(PackageStatus::Missing) => {
+                        ctx.emit(Error::PackageNotRecognized(
+                            package.name.inner,
+                            package.name.span,
+                        ));
+                        degraded = true;
+                    }
+                    Err(err) => {
+                        ctx.emit(Error::Eyre(err, package.name.span));
+                        degraded = true;
+                    }
+                }
+            }
+        }
+
+        if !degraded && !params.is_degraded() {
+            Ok(Transaction {
+                user,
+                packages: packs,
+            })
+        } else {
+            Err(())
+        }
     }
 
     fn install(
         &mut self,
-        _transaction: &Self::Transaction,
-        _ctx: ExecutionCtx,
+        transaction: &Self::Transaction,
+        ctx: ExecutionCtx,
     ) -> (super::OpResult, Self::State) {
-        todo!()
+        let res = flatpak_op(Operation::Install, transaction, ctx);
+        (res, ())
     }
 
     fn remove(
         &mut self,
-        _transaction: &Self::Transaction,
+        transaction: &Self::Transaction,
         _state: Option<Self::State>,
-        _ctx: ExecutionCtx,
+        ctx: ExecutionCtx,
     ) -> super::OpResult {
-        todo!()
+        flatpak_op(Operation::Remove, transaction, ctx)
+    }
+}
+
+enum Operation {
+    Install,
+    Remove,
+}
+
+fn flatpak_op(
+    operation: Operation,
+    transaction: &Transaction,
+    mut ctx: ExecutionCtx,
+) -> super::OpResult {
+    let op = match operation {
+        Operation::Install => "install",
+        Operation::Remove => "remove",
+    };
+    let mut cmd = Command::new("flatpak");
+    cmd.arg(op);
+    if transaction.user {
+        cmd.arg("--user");
+    }
+    let mut proc = cmd
+        .arg("--noninteractive")
+        .args(transaction.packages.iter().map(|p| &p.name))
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .wrap_err("Failed to run flatpak")?;
+    let mut stdout = BufReader::new(proc.stdout.take().unwrap());
+    let mut buf = String::new();
+
+    let res = loop {
+        buf.clear();
+        match stdout.read_line(&mut buf) {
+            //EOF
+            Ok(0) => break Ok(()),
+            Ok(_) => {
+                let mut msg = buf.split(':');
+                match msg.next() {
+                    Some("Info") => tracing::debug!("flatpak: {}", msg.next().unwrap()),
+                    Some(s) if s.starts_with("Installing") || s.starts_with("Uninstalling") => {
+                        ctx.set_message(&s);
+                    }
+                    _ => (),
+                }
+            }
+            Err(err) => break Err(err),
+        }
+    };
+    let output = proc
+        .wait_with_output()
+        .map_err(common::command::Error::IO)
+        .and_then(check_output)
+        .map_err(|e| e.into_report());
+    match (output, res) {
+        (Err(e1), Err(e2)) => Err(e1).error(e2),
+        (Err(err), _) => Err(err),
+        (_, Err(err)) => Err(eyre!(err)),
+        _ => Ok(()),
+    }
+}
+
+enum PackageStatus {
+    Ok,
+    Ambiguous,
+    Missing,
+}
+
+/// Check if a package exists
+fn exists(name: &str, user: bool) -> color_eyre::Result<PackageStatus> {
+    let mut cmd = Command::new("flatpak");
+    cmd.arg("search")
+        .arg(name)
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped());
+    if user {
+        cmd.arg("--user");
+    };
+
+    match run_command(&mut cmd) {
+        Ok(output) => {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            let mut lines = stdout.lines();
+            match (lines.next(), lines.next()) {
+                // No output
+                (None, None) => bail!("Flatpak returned no output"),
+                // No matches
+                (Some("No matches found"), None) => Ok(PackageStatus::Missing),
+                // Just one package
+                (Some(_), None) => Ok(PackageStatus::Ok),
+                // Multiple packages; ambiguous
+                (Some(_), Some(_)) => Ok(PackageStatus::Ambiguous),
+                _ => unreachable!(),
+            }
+        }
+        Err(err) => Err(err.into_report()).wrap_err("Failed to run flatpak"),
     }
 }
